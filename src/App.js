cbc63ea9d4ed7de47e296afe356ebee6977f33b7
LivingstoneDoc <livingstone.doc@yandex.ru>
2023-08-24 20:49:38 +0400

    feat: add header and styles for header. Add images and icons

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="wrapper">
+      <header className="header">
+        <div className="headerLeft">
+          <img src="./icon/header/logo.svg" alt="logo" className="logo" />
+          <div className="headerInfo">
+            <h1>React sneakers</h1>
+            <p>Магазин лучших кроссовок</p>
+          </div>
+        </div>
+        <div className="headerRight">
+          <div className="cart-wrapper">
+            <img src="./icon/header/cart.svg" alt="cart" className="cart" />
+            <p>1205 руб.</p>
+          </div>
+          <img src="./icon/header/favourites.svg" alt="favourites" className="favourites" />
+          <img src="./icon/header/profile.svg" alt="profile" className="profile" />
+        </div>
       </header>
     </div>
   );
