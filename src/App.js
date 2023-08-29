a1ac0e852b73f58a6193942044bf09aebe131196
LivingstoneDoc <livingstone.doc@yandex.ru>
2023-08-29 21:54:35 +0400

    feat: add props

--- src/App.js
+++ src/App.js
@@ -3,9 +3,11 @@ import Cart from "./components/Cart";
 import Card from "./components/Card";
 
 function App() {
-  const sneakersItemCardsArr = [
-    {name: "Мужские Кроссовки Nike Blazer Mid Suede", price: 12999},
-    {name: "Мужские Кроссовки Nike Air Max 270", price: 9999}
+  const sneakersCardsArr = [
+    {title: "Мужские Кроссовки Nike Blazer Mid Suede", price: 12999, imgUrl: "./img/sneakers/sneakers1.png"},
+    {title: "Мужские Кроссовки Nike Air Max 270", price: 9999, imgUrl: "./img/sneakers/sneakers2.png"},
+    {title: "Мужские Кроссовки Nike Blazer Mid Suede", price: 8499, imgUrl: "./img/sneakers/sneakers3.png"},
+    {title: "Кроссовки Puma X Aka Boku Future Rider", price: 8999, imgUrl: "./img/sneakers/sneakers4.png"}
   ]
   return (
     <div className="wrapper">
@@ -20,7 +22,13 @@ function App() {
           </div>
         </div>
         <div className="contentWrapper">
-          {sneakersItemCardsArr.map(arrItem => <Card />)}
+            {sneakersCardsArr.map(itemArr => (
+              <Card 
+                title={itemArr.title}
+                price={itemArr.price}
+                imgUrl={itemArr.imgUrl}
+              />
+            ))}
         </div>
 
       </main>
@@ -29,13 +37,3 @@ function App() {
 }
 
 export default App;
-
-
-
-
-
-
-const sneakersCardsArr = [
-  {name: "Мужские Кроссовки Nike Blazer Mid Suede", price: 12999},
-  {name: "Мужские Кроссовки Nike Air Max 270", price: 9999}
-]
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,15 +1,15 @@
-function Card() {
+function Card(props) {
     return (
         <div className="contentItem">
             <div className="notFavouriteImg">
               <img src="./icon/main-content/favourites-inactive.svg" alt="Not favourite" />
             </div>
-            <img src="./img/sneakers/sneakers1.png" alt="sneakers img" className="sneakersImg" />
-            <p className="contentItemDescr">Мужские Кроссовки Nike Blazer Mid Suede</p>
+            <img src={props.imgUrl} alt="sneakers img" className="sneakersImg" />
+            <p className="contentItemDescr">{props.title}</p>
             <div className="contentItemBottom">
               <div className="price">
                 <p className="priceTitle">Цена:</p>
-                <p className="priceValue">12 999 руб.</p>
+                <p className="priceValue">{props.price} руб.</p>
               </div>
               <img src="./icon/main-content/cart-not-add.svg" alt="add cart" />
             </div>
